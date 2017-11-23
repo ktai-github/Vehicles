a1866a5a29bfbed82b4e697d4a574525eafff124
KevinT <kevin.tai.mail@gmail.com>
2017-11-22 18:47:15 -0800

    Initial Commit

---
+++ Vehicles/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Vehicles
+//
+//  Created by KevinT on 2017-11-22.
+//  Copyright © 2017 kevint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
